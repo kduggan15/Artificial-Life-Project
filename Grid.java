d5e09c5fb3957ed59c059bff56045d3dcfa3bc39
V-Grigoryan <34287235+V-Grigoryan@users.noreply.github.com>
2017-12-21 13:41:20 -0500

    Added daytime() function

--- Grid.java
+++ Grid.java
@@ -7,9 +7,65 @@ public class Grid
 {
     private final int GRIDSIZE = 32;
     private Organism [][] board = new Organism[GRIDSIZE][GRIDSIZE];
-    private HashSet<GridObject> acted = new HashSet<>();
+    // private HashSet<GridObject> acted = new HashSet<>();
     private int turnCount;
     private boolean paused;
+    
+    public void daytime()
+    {
+        // Initialize ArrayList mustAct
+        ArrayList<Organism> mustAct = newArrayList<Organism>();
+        
+        // Carnivores are gathered, then act
+        for(int i = 0; i < GRIDSIZE; i++)
+        {
+            for(int j = 0; j < GRIDSIZE; j++)
+            {
+                if(board[i][j] instanceof Carnivore)
+                    mustAct.add(board[i][j]);
+            }
+        }
+        while(!mustAct.isEmpty())
+        {
+            mustAct.get(0).act();
+            mustAct.remove(0);
+        }
+        
+        // Herbivores that survived the Carnivore action are gathered, then act
+        for(int i = 0; i < GRIDSIZE; i++)
+        {
+            for(int j = 0; j < GRIDSIZE; j++)
+            {
+                if(board[i][j] instanceof Herbivore)
+                    mustAct.add(board[i][j]);
+            }
+        }
+        while(!mustAct.isEmpty())
+        {
+            mustAct.get(0).act();
+            mustAct.remove(0);
+        }
+        
+        // Plants that were not devoured by Herbivores age
+        for(int i = 0; i < GRIDSIZE; i++)
+        {
+            for(int j = 0; j < GRIDSIZE; j++)
+            {
+                if(board[i][j] instanceof Plant)
+                    mustAct.add(board[i][j]);
+            }
+        }
+        while(!mustAct.isEmpty())
+        {
+            mustAct.get(0).act();
+            mustAct.remove(0);
+        }
+        
+        // Day count incremented
+        turnCount += 1;
+    }
+        
+            
     
     @Override
     public String toString()

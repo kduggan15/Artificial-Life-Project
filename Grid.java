48159b64d47eead6e64cc93a4ca17e76f138d314
V-Grigoryan <34287235+V-Grigoryan@users.noreply.github.com>
2017-12-20 21:42:07 -0500

    Added toString() method

--- Grid.java
+++ Grid.java
@@ -6,9 +6,29 @@ import java.util.Random;
 public class Grid
 {
     private final int GRIDSIZE = 32;
-    private GridObject [][] board = new GridObject[GRIDSIZE][GRIDSIZE];
+    private Organism [][] board = new Organism[GRIDSIZE][GRIDSIZE];
     private HashSet<GridObject> acted = new HashSet<>();
     private int turnCount;
     private boolean paused;
     
+    @Override
+    public String toString()
+    {
+        String output = "Day " + turncount + "\n";
+        for(int i = 0; i < GRIDSIZE; i++)
+            output += "--";
+        output += "\n";
+        for(int i = 0; i < GRIDSIZE; i++)
+        {
+            for(int j = 0; j < GRIDSIZE; j++)
+            {
+                if(board[i][j] instanceof Organism)
+                    output += board[i][j].symbol + " ";
+                else
+                    output += "  ";
+            }
+            output += "\n";
+        }
+        return output;
+    }
 }

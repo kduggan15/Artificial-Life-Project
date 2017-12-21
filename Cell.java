ad6fd8777e84d83f83eb582beebba68b64d4b404
V-Grigoryan <34287235+V-Grigoryan@users.noreply.github.com>
2017-12-21 18:02:14 -0500

    Added location variable to Cell

--- Cell.java
+++ Cell.java
@@ -4,12 +4,14 @@ public class Cell
     public enum Terrain { PLAINS, MOUNTAINS, WATER; }
     private final Grid myGrid;
     private final Terrain terrain;
+    private final Location location;
     private Organism inhabitant;
 
-    public Cell(Grid myGrid, Terrain terrain)
+    public Cell(Grid myGrid, Terrain terrain, Location location)
     {
         this.myGrid = myGrid;
         this.terrain = terrain;
+        this.location = location;
         inhabitant = null;
     }
 

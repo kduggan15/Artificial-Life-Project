53add40009859ab2347d6467547218075137ad35
V-Grigoryan <34287235+V-Grigoryan@users.noreply.github.com>
2017-12-22 13:23:53 -0500

    Added comments.

--- DNA.java
+++ DNA.java
@@ -5,6 +5,7 @@ public class DNA
     private final double dailyEnergyMultiplier;
     private final double lifeSpanMultiplier;
 
+    // Generates random DNA.
     public DNA()
     {
         Random random = new Random();
@@ -12,22 +13,26 @@ public class DNA
         lifeSpanMultiplier = ( 70 + random.nextInt(60) ) / 100.0;
     }
 
+    // Generates DNA based on the DNA of two parents.
     public DNA(DNA parent1, DNA parent2)
     {
         dailyEnergyMultiplier = (parent1.getDailyEnergyMultiplier() + parent2.getDailyEnergyMultiplier()) / 2;
         lifeSpanMultiplier = (parent1.getLifeSpanMultiplier() + parent2.getLifeSpanMultiplier()) / 2;
     }
 
+    // Returns the daily energyToAct multiplier.
     public double getDailyEnergyMultiplier()
     {
         return dailyEnergyMultiplier;
     }
 
+    // Returns the life span multiplier.
     public double getLifeSpanMultiplier()
     {
         return lifeSpanMultiplier;
     }
 
+    // Computes the strength of this DNA.
     public double getDNAStrength()
     {
         return (dailyEnergyMultiplier + lifeSpanMultiplier) / 2;
